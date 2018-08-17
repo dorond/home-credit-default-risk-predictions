54277037c7367f86d0a7599e07893a0769810ce0
Doron Dusheiko <doron.dusheiko@gmail.com>
2018-08-17 11:10:03 +0200

    initial commit of project structure, including literature

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict which loans will default',
+    author='Doron Dusheiko',
+    license='MIT',
+)
